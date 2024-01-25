82ec653fe70afee1139c5506000dc3140587c070
SamuelWish <viwnya08@gmail.com>
2024-01-26 00:52:10 +0700

    search astro in words

---
+++ search_astro.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <clocale>
+#include <string.h>
+#define LISTSIZE 5
+
+int main(void)
+{
+	setlocale(LC_ALL, "Russian"); //Добавим русский язык
+	const char* list[LISTSIZE] =
+	{
+		"astronomy", " astounding", "astrophysics", "ostracize", "asterism"
+	};
+	int count = 0;
+	int i;
+	for (i = 0; i < LISTSIZE;i++)
+		if (strncmp(list[i], "astro", 5) == 0)
+		{
+			printf("has been found: %s\n", list[i]);
+			count++;
+		}
+	printf("list contains %d words that start with astro.\n", count);
+
+	return 0;
+}
+
